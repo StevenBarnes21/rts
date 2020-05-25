6ac51bd171d04d1d8151fe56aa5b41ec46bb2232
Steven Barnes <ssbarnes21@gmail.com>
2020-05-25 18:39:04 +0100

    Added building collision detection when adding buildings

--- js/factories.js
+++ js/factories.js
@@ -13,8 +13,8 @@ function Building(x, y, w, h, color) {
 
 let currentlySelected = null;
 house.addEventListener('click', (e) => {
-  currentlySelected = 'house';
-  console.log(currentlySelected);
+  currentlySelected = new Building(null, null, 30, 30, '#00F');
+  console.log('line 17',currentlySelected);
 });
 
 let mouseX = 150;
@@ -23,25 +23,25 @@ const mainCanvas = document.getElementById('main-canvas');
 mainCanvas.width = 500;
 mainCanvas.height = 500;
 
+// Updates the mouse position
 mainCanvas.addEventListener('mousemove', (e) => {
   mouseX = e.offsetX;
   mouseY = e.offsetY;
-  console.log(mouseX, mouseY);
 });
 
+// Adds a building if one has already been selected
 mainCanvas.addEventListener('click', (e)=> {
   if(currentlySelected) {
-    gameBuildings.push(new Building(mouseX, mouseY, 20, 20, '#00F'));
-    currentlySelected = null;
+    currentlySelected.x = mouseX;
+    currentlySelected.y = mouseY;
+    let added = addBuilding(currentlySelected);
+
+    if(added) 
+      currentlySelected = null;
   }
 });
 
 const ctx = mainCanvas.getContext('2d');
-ctx.beginPath();
-ctx.fillStyle = '#00F';
-ctx.fillRect(50, 50, 50, 50);
-console.log(mouseX);
-ctx.fill();
 
 (function gameLoop() {
 
@@ -49,8 +49,8 @@ ctx.fill();
 
   drawBuidings();
 
+  // Show the outline of the building before it is placed
   if(currentlySelected) {
-    console.log('selected');
     ctx.beginPath();
     ctx.strokeStyle = '#00F';
     ctx.strokeRect(mouseX, mouseY, 30, 30);
@@ -68,9 +68,64 @@ function drawBuidings() {
     ctx.fillRect(gameBuildings[i].x, 
                  gameBuildings[i].y, 
                  gameBuildings[i].w, 
-                 gameBuildings[i].h);
-    
+                 gameBuildings[i].h); 
+  }
+}
+
+function addBuilding(building) {
+ 
+  // Edge case: There are no buildings added yet 
+  if(gameBuildings.length === 0) {
+    gameBuildings.push(building);
+    return true;
   }
 
+  // Loop over every building and check no buildings collide with this one
+  for(let i = 0; i < gameBuildings.length; i++) {
+    if(collides(building, gameBuildings[i])) {
+      console.log('collides');
+      return false;    
+    }
+  }
 
+  // If the code reaches this part then no collisions have occurred.
+  gameBuildings.push(building);
+  return true;
 }
+
+function collides(objA, objB) {
+
+  // If objA clears both the left and right sides of objB then y does not matter
+  if( (objA.x + objA.w) < objB.x || objA.x > (objB.x + objB.w) ){
+    console.log('clears x');
+    return false;
+  } 
+
+  // If objA clears both the top and bottom sides of objB then x does not matter 
+  if( (objA.y + objA.h) < objB.y || objA.y > (objB.y + objB.h) ) {
+    console.log('clears y');
+    return false;
+  } 
+
+  return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

7bc647e74eccf1b81023af1013f51ee79a3f6795
Steven Barnes <ssbarnes21@gmail.com>
2020-05-25 17:44:17 +0100

    Added house

---
+++ js/factories.js
@@ -0,0 +1,67 @@
+"use strict"
+
+const house = document.getElementById('house');
+const gameBuildings = [];
+
+function Building(x, y, w, h, color) {
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.color = color;
+}
+
+let currentlySelected = null;
+house.addEventListener('click', (e) => {
+  currentlySelected = 'house';
+  console.log(currentlySelected);
+});
+
+let mouseX = 150;
+let mouseY = 150;
+const mainCanvas = document.getElementById('main-canvas');
+mainCanvas.width = 500;
+mainCanvas.height = 500;
+
+mainCanvas.addEventListener('mousemove', (e) => {
+  mouseX = e.offsetX;
+  mouseY = e.offsetY;
+  console.log(mouseX, mouseY);
+});
+
+mainCanvas.addEventListener('click', (e)=> {
+  if(currentlySelected) {
+    gameBuildings.push(new Building(mouseX, mouseY, 20, 20, '#00F'));
+    currentlySelected = null;
+  }
+});
+
+const ctx = mainCanvas.getContext('2d');
+ctx.beginPath();
+ctx.fillStyle = '#00F';
+ctx.fillRect(50, 50, 50, 50);
+console.log(mouseX);
+ctx.fill();
+
+(function gameLoop() {
+
+  ctx.clearRect(0,0,500,500);
+
+
+  if(currentlySelected) {
+    console.log('selected');
+    ctx.beginPath();
+    ctx.strokeStyle = '#00F';
+    ctx.strokeRect(mouseX, mouseY, 30, 30);
+    ctx.stroke();
+  }
+
+  requestAnimationFrame(gameLoop);
+})();
+
+function drawBuidings() {
+
+  ctx.fillStyle = '#00F';
+
+
+}
